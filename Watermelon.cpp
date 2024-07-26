6a5dd50543c742c51292c513b6877ab9acb78b4b
Mohammed <HNU.24.308.Muhammad-Abdulnabi@hnu.edu.eg>
2024-07-26 08:24:55 +0300

    solution of some problems on codeforces problem set

---
+++ Watermelon.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fastIO()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main()
+{
+    fastIO();
+    int w;
+    cin >> w;
+
+    if (w % 2 == 0 && w > 2)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
---
+++ Way_Too_Long_Words.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fastIO()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main()
+{
+    fastIO();
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        string str;
+        cin >> str;
+
+        if (str.length() > 10)
+        {
+            cout << str[0] << str.length() - 2 << str[str.length() - 1] << endl;
+        }
+        else
+        {
+            cout << str << endl;
+        }
+    }
+
+    return 0;
+}
