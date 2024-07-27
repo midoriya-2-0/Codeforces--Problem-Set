95d177c7b22d8fecf8f821165a6330f23e2ff610
Mohammed <HNU.24.308.Muhammad-Abdulnabi@hnu.edu.eg>
2024-07-27 15:56:01 +0300

    solution of some problems on codeforces problem set

---
+++ Helpful_Maths.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fastOI()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main()
+{
+    fastOI();
+
+    string s;
+    cin >> s;
+
+    vector<int> v;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] != '+')
+        {
+            v.push_back(s[i] - '0');
+        }
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i];
+        if (i != v.size() - 1)
+        {
+            cout << "+";
+        }
+    }
+    cout << endl;
+}
